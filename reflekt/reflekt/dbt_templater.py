ace40cd731a2fff07af86e5384c0b5168926bb91
Greg Clunies <greg.clunies@gmail.com>
2022-04-30 20:18:15 -0700

    feat: add template_dbt_config()

--- reflekt/reflekt/dbt_templater.py
+++ reflekt/reflekt/dbt_templater.py
@@ -2,12 +2,39 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+from loguru import logger
+from reflekt.logger import logger_config
+from reflekt.reflekt.errors import ReflektProjectError
 
-def template_dbt_config():
-    pass
+
+def template_dbt_config(id, materialized, warehouse_type):
+    if materialized == "view":
+        config_str = "{{\n" "  config(\n" "    materialized = 'view',\n" "  )\n" "}}\n\n"
+    elif materialized == "incremental":
+        if warehouse_type == "redshift":
+            sort_str = "sort = 'tstamp'"
+        elif warehouse_type == "snowflake":
+            sort_str = "cluster_by = 'tstamp'"
+        config_str = (
+            "{{\n"
+            f"  config(\n"
+            f"    materialized = 'incremental',\n"
+            f"    unique_key = '{id}',\n"
+            f"    {sort_str}\n"
+            f"  )\n"
+            "}}\n\n"
+        )
+    else:
+        raise ReflektProjectError(
+            f"Invalid materialized config in reflekt_project.yml...\n"
+            f"    materialized: {materialized}\n"
+            f"... is not accepted. Must be either 'view' or 'incremental'."
+        )
+
+    return config_str
 
 
-def template_dbt_source_cte():
+def template_dbt_source_cte(materialized):
     pass
 
 
@@ -19,7 +46,7 @@ def template_dbt_std_columns():
     pass
 
 
-def template_dbt_event_columns():
+def template_dbt_unique_columns():
     pass
 
 
