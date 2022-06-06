a20363a5cdc93706edf223a87ae91052cf4a8c62
Greg Clunies <greg.clunies@gmail.com>
2022-06-06 12:29:39 -0700

    chore: add git tagging prompt for reflekt dbt back in. Issu was bad import

--- reflekt/reflekt/cli.py
+++ reflekt/reflekt/cli.py
@@ -4,8 +4,8 @@
 
 import json
 import shutil
+import subprocess
 import typing
-from asyncio import subprocess
 from pathlib import Path
 
 import click
@@ -539,6 +539,28 @@ def dbt(
         pkg_version=version,
     )
     transformer.build_dbt_package()
+
+    create_tag = click.confirm(
+        f"Would you like to create a Git tag to easily reference Reflekt dbt package "
+        f"{pkg_name} (version: {str(version)}) in your dbt project?",
+        default=False,
+    )
+
+    if create_tag:
+        tag = click.prompt(
+            "Tag",
+            type=str,
+            default=f"v{str(version)}_{pkg_name}",
+        )
+        git_executable = shutil.which("git")
+        subprocess.call(
+            args=[
+                git_executable,
+                "tag",
+                tag,
+            ],
+            cwd=project_dir,
+        )
 
 
 # Add CLI commands to CLI group
