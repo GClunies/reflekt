6f5d71bd8ca991f52add230ea85977022e857375
Greg Clunies <greg.clunies@gmail.com>
2023-02-07 00:29:09 -0500

    CI: test pypi workflows (#64)
    * update workflows for CI suite
    
    * update gitignore again
    
    * specify python and poetry versions in workflow
    
    * update release workflow

---
+++ artifacts/dbt/reflekt_demo/models/ecomm_demo/stg_ecomm_demo__tracks.sql
@@ -0,0 +1,38 @@
+{{
+  config(
+    materialized = 'view'
+  )
+}}
+
+with
+
+source as (
+    select * from {{ source('ecomm_demo', 'tracks') }}
+),
+
+renamed as (
+    select
+        id as event_id,
+        event_text as event_name,
+        original_timestamp as original_tstamp,
+        sent_at as sent_at_tstamp,
+        received_at as received_at_tstamp,
+        timestamp as tstamp,
+        anonymous_id,
+        user_id,
+        context_ip as ip,
+        context_library_name as library_name,
+        context_library_version as library_version,
+        context_page_path as page_path,
+        context_page_referrer as page_referrer,
+        context_page_title as page_title,
+        context_page_url as page_url,
+        context_user_agent as user_agent,
+        'track'::varchar as call_type,
+        'ecomm_demo'::varchar as source_schema,
+        'tracks'::varchar as source_table,
+        'segment/ecommerce/Product Added/1-0.json'::varchar as schema_id
+    from source
+)
+
+select * from renamed
---
+++ artifacts/dbt/reflekt_demo/models/ecomm_demo/stg_ecomm_demo__users.sql
@@ -0,0 +1,33 @@
+{{
+  config(
+    materialized = 'view'
+  )
+}}
+
+with
+
+source as (
+    select * from {{ source('ecomm_demo', 'users') }}
+),
+
+renamed as (
+    select
+        id as user_id,
+        received_at as received_at_tstamp,
+        context_ip as ip,
+        context_library_name as library_name,
+        context_library_version as library_version,
+        context_page_path as page_path,
+        context_page_referrer as page_referrer,
+        context_page_search as page_search,
+        context_page_title as page_title,
+        context_page_url as page_url,
+        context_user_agent as user_agent,
+        'identify'::varchar as call_type,
+        'ecomm_demo'::varchar as source_schema,
+        'users'::varchar as source_table,
+        'segment/ecommerce/Identify/1-0.json'::varchar as schema_id
+    from source
+)
+
+select * from renamed
